739ac7533357c9f5374818f0b7b25a34db7e6a4f
sarbador <avaz.shams.2002@gmail.com>
2025-03-01 16:06:16 +0500

    separated gorouter from materialcontext

---
+++ lib/go_router_src/common/router/gr_router.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:routing/go_router_src/features/cart/widgets/cart_gr_widget.dart';
+import 'package:routing/go_router_src/features/category/widgets/category_gr_widget.dart';
+import 'package:routing/go_router_src/features/home_gr.dart';
+import 'package:routing/go_router_src/features/product/widgets/product_gr_widget.dart';
+
+mixin GrRouter<T extends StatefulWidget> on State<T> {
+  late final GoRouter goRouter;
+
+  @override
+  void initState() {
+    super.initState();
+    goRouter = GoRouter(
+      initialLocation: "/categories",
+      routes: [
+        ShellRoute(
+          routes: [
+            GoRoute(
+              path: "/categories",
+              routes: [
+                GoRoute(
+                  path: "products/:productId",
+                  builder: (context, state) {
+                    return ProductGrWidget(
+                      productId: state.pathParameters['productId'] as String,
+                    );
+                  },
+                ),
+              ],
+              builder: (context, state) => CategoryGrWidget(),
+            ),
+            GoRoute(
+              path: "/cart",
+              builder: (context, state) => CartGrWidget(),
+            ),
+          ],
+          builder: (context, state, child) {
+            return HomeGr(child: child);
+          },
+        ),
+      ],
+    );
+  }
+}
--- lib/go_router_src/features/init/widgets/material_gr_context.dart
+++ lib/go_router_src/features/init/widgets/material_gr_context.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:routing/go_router_src/features/cart/widgets/cart_gr_widget.dart';
-import 'package:routing/go_router_src/features/category/widgets/category_gr_widget.dart';
-import 'package:routing/go_router_src/features/home_gr.dart';
-import 'package:routing/go_router_src/features/product/widgets/product_gr_widget.dart';
+import 'package:routing/go_router_src/common/router/gr_router.dart';
 
 class MaterialGrContextApp extends StatelessWidget {
   const MaterialGrContextApp({super.key});
@@ -21,40 +17,11 @@ class MaterialGrContext extends StatefulWidget {
   State<MaterialGrContext> createState() => _MaterialGrContextState();
 }
 
-class _MaterialGrContextState extends State<MaterialGrContext> {
+class _MaterialGrContextState extends State<MaterialGrContext> with GrRouter {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: GoRouter(
-        initialLocation: "/categories",
-        routes: [
-          ShellRoute(
-            routes: [
-              GoRoute(
-                path: "/categories",
-                routes: [
-                  GoRoute(
-                    path: "products/:productId",
-                    builder: (context, state) {
-                      return ProductGrWidget(
-                        productId: state.pathParameters['productId'] as String,
-                      );
-                    },
-                  ),
-                ],
-                builder: (context, state) => CategoryGrWidget(),
-              ),
-              GoRoute(
-                path: "/cart",
-                builder: (context, state) => CartGrWidget(),
-              ),
-            ],
-            builder: (context, state, child) {
-              return HomeGr(child: child);
-            },
-          ),
-        ],
-      ),
+      routerConfig: goRouter,
     );
   }
 }
